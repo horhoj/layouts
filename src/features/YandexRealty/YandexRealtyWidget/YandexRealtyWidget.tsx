6449c6a850fa46d4a2ffd397042e785d01fc37f1
horhoj <horhoj@yandex.ru>
2021-12-11 17:00:10 +0500

    simplified the scrolling algorithm for YandexRealtyWidget

--- src/features/YandexRealty/YandexRealtyWidget/YandexRealtyWidget.tsx
+++ src/features/YandexRealty/YandexRealtyWidget/YandexRealtyWidget.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useRef, useState } from 'react';
 import styles from './YandexRealtyWidget.module.scss';
 import { TitleBlock } from './TitleBlock';
 import { FilterBlock } from './FilterBlock';
@@ -9,27 +9,18 @@ import { CardList } from './CardList';
 export const YandexRealtyWidget: FC = () => {
   const [currentFilter, setCurrentFilter] =
     useState<DataFilterItemValue>('new');
-  const [scrollStep, setScrollStep] = useState<number>(0);
+
   const cardListRef = useRef<HTMLDivElement>(null);
 
   const dataCardListFiltered = dataCardList.filter(
     (dataCardItem) => dataCardItem.dataFilterValue === currentFilter,
   );
 
-  // const dataCardListFiltered = dataCardList;
-
-  useEffect(() => {
-    if (cardListRef.current) {
-      const scrollWidth = cardListRef.current.scrollWidth;
-      const newScrollStep = scrollWidth / dataCardListFiltered.length;
-      setScrollStep(newScrollStep);
-      cardListRef.current.scrollBy({ left: -scrollWidth, behavior: 'smooth' });
-    }
-  }, [currentFilter]);
-
   const handleScrollBack = () => {
     if (cardListRef.current) {
       const currentScrollOffset = cardListRef.current.scrollLeft;
+      const scrollWidth = cardListRef.current.scrollWidth;
+      const scrollStep = scrollWidth / dataCardListFiltered.length;
       const newScrollOffset = Math.floor(currentScrollOffset % scrollStep);
       cardListRef.current.scrollBy({
         left: -(newScrollOffset === 0 ? scrollStep : newScrollOffset),
@@ -42,6 +33,8 @@ export const YandexRealtyWidget: FC = () => {
   const handleScrollForward = () => {
     if (cardListRef.current) {
       const currentScrollOffset = cardListRef.current.scrollLeft;
+      const scrollWidth = cardListRef.current.scrollWidth;
+      const scrollStep = scrollWidth / dataCardListFiltered.length;
       const newScrollOffset =
         scrollStep - Math.floor(currentScrollOffset % scrollStep);
       cardListRef.current.scrollBy({
@@ -65,12 +58,7 @@ export const YandexRealtyWidget: FC = () => {
         />
       </div>
       <div className={styles.cardListWrap}>
-        <CardList
-          dataCardList={dataCardListFiltered}
-          // scroll={scroll}
-          // setScrollStep={setScrollStep}
-          ref={cardListRef}
-        />
+        <CardList dataCardList={dataCardListFiltered} ref={cardListRef} />
       </div>
       <div
         className={`${styles.scrollButton} ${styles.scrollButtonBack}`}
